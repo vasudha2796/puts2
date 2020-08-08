ecd9c163e18960789f7a1c0318a0fc64a1664d5c
Vasudha Dasari <vasudha.dasari@yahoo.com>
2020-08-09 01:54:52 +0200

    Added Median method and its test cases to Median Branch

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import statistics
+
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -14,6 +16,27 @@ def addition():
     value2 = request.args.get('B', default=0, type=int)
     result = value1 + value2
     return '%d \n' % result
+
+
+@app.route('/median')
+def median():
+    value = []
+    value1 = str(request.args.get('A'))
+    value1 = value1.split(',')
+    try:
+        value1.sort()
+        for i in value1:
+            i = float(i)
+            value.append(i)
+
+        med = statistics.median(value)
+        if med.is_integer():
+            return '%d \n' % med
+        else:
+            return '%.3f \n' % med
+    except ValueError:
+        warning = "Enter numbers in the correct format"
+        return warning
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -10,9 +10,37 @@ class TestCaseForCalculator(unittest.TestCase):
         self.app = main.app.test_client()
 
     def test_empty_page(self):
-
         response_data = self.app.get('/')
         self.assertEqual(b'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n', response_data.data)
+
+    def test_median(self):
+        # when A contains integer values and even digits
+        response_data = self.app.get('/median?A=5,3,10,15')
+        self.assertEqual(b'7.500 \n', response_data.data)
+
+        # when A contains float values and even digits
+        response_data = self.app.get('/median?A=4.2,3.1,1.4,15.3')
+        self.assertEqual(b'3.650 \n', response_data.data)
+
+        # when A contains both integer and float values and having even digits
+        response_data = self.app.get('/median?A=5,3.2,10.5,15')
+        self.assertEqual(b'7.750 \n', response_data.data)
+
+        # when A contains integer values and odd digits
+        response_data = self.app.get('/median?A=5,3,1,10,15')
+        self.assertEqual(b'5 \n', response_data.data)
+
+        # when A contains float values and odd digits
+        response_data = self.app.get('/median?A=4.2,3.1,1.4,11.2,15.3')
+        self.assertEqual(b'4.200 \n', response_data.data)
+
+        # when A contains both integer and float values and having odd digits
+        response_data = self.app.get('/median?A=5,3.2,1.4,10,15')
+        self.assertEqual(b'5 \n', response_data.data)
+
+        # When A is non number type
+        response_data = self.app.get('/median?A=5,3.2,c,n,1.4,10,15')
+        self.assertEqual(b"Enter numbers in the correct format", response_data.data)
 
 
 if __name__ == '__main__':
